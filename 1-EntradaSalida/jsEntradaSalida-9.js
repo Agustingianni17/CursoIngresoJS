cce86e14b1a1a8b70d05f8571c66cc5621ef88b8
Agustingianni17 <60628756+Agustingianni17@users.noreply.github.com>
2020-02-05 14:49:27 -0300

    entrada y salida 9
    el importe con un aumento del 10 %

--- 1-EntradaSalida/jsEntradaSalida-9.js
+++ 1-EntradaSalida/jsEntradaSalida-9.js
@@ -4,6 +4,25 @@ mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function mostrarAumento()
 {
+	var ingresosueldo;
+	var aumento;
+	var suma;
 
+
+	ingresosueldo= document.getElementById('sueldo').value;
+	ingresosueldo=parseInt(ingresosueldo);
+
+	aumento = ingresosueldo / 10;
+
+	suma = ingresosueldo + aumento;
+
+
+	console.log(suma);
+   
+   document.getElementById('resultado').value = suma;
 	
+    
+  
+
+
 }
