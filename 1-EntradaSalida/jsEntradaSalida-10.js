d88fe8b863145320e7bcffc073a5eb01293dd886
Agustingianni17 <60628756+Agustingianni17@users.noreply.github.com>
2020-02-05 14:51:11 -0300

    commit 11
    20% de descuento

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -4,5 +4,25 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function mostrarAumento()
 {
-	
+	 
+    var ingresosueldo;
+	var aumento;
+	var suma;
+
+
+	ingresosueldo= document.getElementById('importe').value;
+	ingresosueldo=parseInt(ingresosueldo);
+
+	aumento = ingresosueldo * 0.20 ;
+
+	suma = ingresosueldo - aumento;
+
+
+	console.log(suma);
+   
+   document.getElementById('resultado').value = suma;
+
+
+
+
 }
