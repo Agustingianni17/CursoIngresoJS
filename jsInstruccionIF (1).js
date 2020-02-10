8a83d319c6fd9897c05046ac54a3822f598b0299
Agustingianni17 <60628756+Agustingianni17@users.noreply.github.com>
2020-02-10 20:29:53 -0300

    if 1
    if1

---
+++ jsInstruccionIF (1).js
@@ -0,0 +1,23 @@
+function mostrar()
+{
+     var edad;
+     //tomo la edad 
+
+     edad= document.getElementById('edad').value;
+     
+
+
+     if( edads=="15" )
+     {
+         alert('algo');
+
+
+     }
+
+
+ 
+
+
+
+}
+//FIN DE LA FUNCIÓN
