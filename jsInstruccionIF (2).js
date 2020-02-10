8d2492a0d982c27473c766519c40aeb0da99021d
Agustingianni17 <60628756+Agustingianni17@users.noreply.github.com>
2020-02-10 20:34:34 -0300

    if 2
    if 2

---
+++ jsInstruccionIF (2).js
@@ -0,0 +1,19 @@
+function mostrar()
+{
+	var edad;
+    //tomo la edad  
+
+    edad= document.getElementById('edad').value;
+
+
+
+    if( edad > 17)
+
+    {
+    	alert('sos mayor de edad');
+           
+
+    }
+
+
+}//FIN DE LA FUNCIÓN
