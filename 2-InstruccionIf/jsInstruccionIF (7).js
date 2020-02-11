5a01ae9fddb09d33ec42e324399eb66ecc186eec
Agustingianni17 <60628756+Agustingianni17@users.noreply.github.com>
2020-02-10 21:57:17 -0300

    if 7
    if7

--- 2-InstruccionIf/jsInstruccionIF (7).js
+++ 2-InstruccionIf/jsInstruccionIF (7).js
@@ -1,6 +1,35 @@
 function mostrar()
 {
-//tomo la edad  
+	var edad;
+    var estadoCivil;
+//tomo la edad
+     edad= document.getElementById('edad').value;
+     estadoCivil= document.getElementById('estadoCivil').value;
+
+     if(!(edad<18 && estadoCivil!="Soltero" ))
+     {
+		alert("Es muy pequeño para NO ser soltero")
+     }
+
+
+
+
+      if(edad<18 && (estadoCivil=="Soltero" || estadoCivil=="Divorciado"))
+      if(edad<18 || estadoCivil=="Soltero")//???????
+      {
+      	alert("Es muy pequeño para NO ser soltero")
+      }
+      else{
+
+      
+      }
+
+
+
+
+
+
+
 
 	
 
